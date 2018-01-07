e7d10ca95c08cb6c44f0c15a3217bde5f68a84b9
CRCx86 <crc32zinov@gmail.com>
2018-01-07 12:59:10 +0300

    homework 7

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+public class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+    @Test
+    public void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                //.andExpect(contentJson(MEAL1))
+        ;
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testGetAll() {
+    }
+
+    @Test
+    public void testCreateWithLocation() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+}
